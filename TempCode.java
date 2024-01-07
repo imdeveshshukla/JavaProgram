f0a2ac740d6976f4b04abd89837a46db391b7d01
Devesh Shukla <deveshshukla1603@gmail.com>
2024-01-07 17:36:52 +0530

    Recursion

--- TempCode.java
+++ TempCode.java
@@ -1,36 +1,17 @@
 import java.util.*;
 class TempCode{
-    static boolean isPalindrome(String s){
-        int k = s.length()-1;
-        for(int i=0;i<(s.length())/2;i++)
-        {
-            if(s.charAt(i)!=s.charAt(k))
-                return false;
-            k--;
+    public static boolean isPowerOfFour(int n) {
+        while (n!=0) {
+            if(n==4)
+                System.out.println("true");
+            n = n>>2;
         }
-        return true;
+        return false;
+
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next(),a = "";
-        int l = s.length();
-        int total = (l*(l+1))/2;
-        String ans[] = new String[total];
-        boolean found = false;
-        int index = 0;
-        for(int i=0;i<s.length();i++){
-            for(int j=s.length();j>i;j--){
-                a = s.substring(i,j);
-                
-                System.out.println(a);
-                if(isPalindrome(a))
-                {
-                    found = true;
-                    ans[index] = a;
-                    index++;
-                }
-            }   
-        }
-        System.out.println(Arrays.toString(ans));
+        int n = 64;
+        System.out.println(isPowerOfFour(n));
+        
     }
 }
